60620e52fb75e3e921121f1e557038baf59e112b
Vladislav <vidga471@gmail.com>
2024-01-30 01:56:19 +0300

    add backbutton in PlaylistsPage.tsx

--- src/pages/PlaylistsPage.tsx
+++ src/pages/PlaylistsPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SelectedMoviesList from '../components/MovieList/SelectedMoviesList';
 import '../styles/PlaylistsPage.css';
 
 const PlaylistsPage: React.FC = () => {
   return (
-    <div className="playlists-page-container">
+    <div className="playlists-page-container playlists-page">
+      <Link to="/" className="back-button">Назад</Link>
       <h1>Избранные фильмы</h1>
       <SelectedMoviesList />
     </div>
