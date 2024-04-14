9d1dd391c4dc21884fde5930239c15feee15b2d4
Vladislav <vidga471@gmail.com>
2024-04-14 12:27:45 +0300

    add .env

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBCllSufTGXb4dplV5c_SH4QamU4WjVRlU',
-  authDomain: 'kino-app-d1569.firebaseapp.com',
-  projectId: 'kino-app-d1569',
-  storageBucket: 'kino-app-d1569.appspot.com',
-  messagingSenderId: '333690872279',
-  appId: '1:333690872279:web:a997036a156f545d2dab92',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
+
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
